1344a3fa00333a516ff57a32f58bc94be5ccf24c
Guilherme Jordao <luiz.moraes@cwi.com.br>
2022-06-29 19:36:43 -0300

    feat: Add metodos de soma e subtracao

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,8 +6,37 @@ namespace Calculator
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
 
+			// Soma();
+			Subtracao();
+
+		}
+
+
+		static void Soma()
+		{
+			Console.Clear();
+			Console.WriteLine("Primeito Valor: ");
+			float v1 = float.Parse(Console.ReadLine());
+			Console.WriteLine("Segundo Valor: ");
+			float v2 = float.Parse(Console.ReadLine());
+			float total = v1 + v2;
+			Console.WriteLine("");
+			Console.WriteLine($"A soma dos valores é {v1 + v2}");
+			Console.ReadKey();
+		}
+
+		static void Subtracao()
+		{
+			Console.Clear();
+			Console.WriteLine("Primeito Valor: ");
+			float v1 = float.Parse(Console.ReadLine());
+			Console.WriteLine("Segundo Valor: ");
+			float v2 = float.Parse(Console.ReadLine());
+			float total = v1 - v2;
+			Console.WriteLine("");
+			Console.WriteLine($"O Resultado da subtração é {v1 - v2}");
+			Console.ReadKey();
 		}
 	}
 }
